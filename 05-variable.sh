5bca8f69f5f8aeee7c102d503f9495097b1d65c0
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-07 17:08:49 +0530

    commiting

--- 05-variable.sh
+++ 05-variable.sh
@@ -3,11 +3,11 @@
 # TIMESTAMP=$(date)
 # echo "time is $TIMESTAMP"
 
-START_TIME=(date +%s)
+START_TIME=$(date +%s)
 
 sleep 20
 
-END_TIME=(date +%s)
+END_TIME=$(date +%s)
 
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
