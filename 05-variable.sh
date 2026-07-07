c10ac5f648e07b9bf3936defe96e0b9ec14755ff
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-07 17:06:25 +0530

    commiting

--- 05-variable.sh
+++ 05-variable.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 TIMESTAMP=$(date)
-echo "time sis $TIMESTAMP"
+echo "time is $TIMESTAMP"
+
+START_TIME=(date +%s)
+
+sleep 20
+
+END_TIME=(date +%s)
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+echo "Total time taken to run the script is $TOTAL_TIME"
