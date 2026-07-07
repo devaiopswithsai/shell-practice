f66c6eceaa130733ff2ab9ef3ef19776f3b11273
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-07 16:51:12 +0530

    commiting

--- 02-converstaion.sh
+++ 02-converstaion.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "Sai:Hi Vrishank, How are you"
-echo "Vrishank:Hi Sai, Am good How are you?"
-echo "Sai:What are you doing"
-echo "Vrishank:Going to school"
+
+PERSON1=sai
+PERSON2=$PERSON2
+
+echo "$PERSON1:Hi $PERSON2, How are you"
+echo "$PERSON2:Hi $PERSON1, Am good How are you?"
+echo "$PERSON1:What are you doing"
+echo "$PERSON2:Going to school"
