8360e2f802bd1fa522289097fda22348a17772de
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-07 16:59:34 +0530

    commiting

--- 04-varibale.sh
+++ 04-varibale.sh
@@ -4,3 +4,7 @@ echo "Please enter username"
 read USER_NAME # here USER_NAME is variable, whatever you enter in terminal stores in this variable
 
 echo "User name is $USER_NAME"
+
+echo "Please enter password"
+read -s PASSWORD
+echo "Password is $PASSWORD"
