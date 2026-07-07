58799857fa6eec55d9f9931baf7134709623e85c
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-07 16:54:55 +0530

    commiting

---
+++ 03-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:Hi $PERSON2, How are you"
+echo "$PERSON2:Hi $PERSON1, Am good How are you?"
+echo "$PERSON1:What are you doing"
+echo "$PERSON2:Going to school"
