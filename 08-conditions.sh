0f7a0098f4297c70dc7502187a721e1b0ab1cfca
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-11 15:21:49 +0530

    commiting

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -4,5 +4,9 @@ NUM=$1
 
 
 if [ $NUM -gt 10 ]; then
-    echo "number is greater than 10"
+    echo "given number $NUM is greater than 10"
+elif [ $NUM -eq 10 ]; then
+     echo "given number $NUM is equal to  10"
+else
+    echo "given number $NUM is less than 10"
 fi
