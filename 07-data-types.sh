c39f8656f46138ad699bf2eb61c878273cd1c0b1
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-11 15:12:12 +0530

    commiting

--- 07-data-types.sh
+++ 07-data-types.sh
@@ -6,3 +6,14 @@ NUMBER2=$2
 SUM=$(($NUMBER1+NUMBER2))
 
 echo "sum is $SUM"
+
+# in shell string also consider as integer only
+
+###Array
+
+Course = ("AWS" "AI" "DEVOPS") # index always starts from zero
+
+echo "Coruses are: ${course[@]}"
+echo "First course is : ${course[1]}"
+echo "second course is : ${course[2]}"
+echo "Third course is : ${course[3]}"
