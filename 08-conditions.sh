59faed419f7d1a7a0b66c677d29211ac37cc9676
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-11 15:23:02 +0530

    commiting

--- 08-conditions.sh
+++ 08-conditions.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 NUM=$1
-
+# -gt greater than
+# lt - less than
+# eq - equal
+# ne - not equal
+# ge - greater than or equal
+# le - less than or equal
 
 if [ $NUM -gt 10 ]; then
     echo "given number $NUM is greater than 10"
