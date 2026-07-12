28010d81438c0c1df307d78b52b39374c7eca870
devaiopswithsai <Chinthasaikumar2079@gmai.com>
2026-07-12 17:08:06 +0530

    commiting

---
+++ 12-loops.sh
@@ -0,0 +1,35 @@
+USERID=$(id -u)
+
+
+if [ $USERID -ne 0 ]; then
+   echo "Please run the script with root access"
+   exit 1
+
+    
+fi
+
+# first arg -> what are you trying to install
+# second arg -> exit code
+VALIDATE() {
+    if [ $2 -ne 0 ]; then
+    echo "installing $1 failed"
+    exit 1
+    else
+
+    echo "installing $1 success"
+    fi
+}
+
+for package in $@
+
+do
+    dnf list installed  $package &>> $LOG_FILE
+
+    if [ $? -eq 0 ]; then
+    echo "$package is already installed SKIPPING" | tee -a $LOG_FILE
+    else
+        echo "Installing $package"
+        dnf install $package -y &>> $LOG_FILE
+        VALIDATE $package $? 
+    fi
+done
